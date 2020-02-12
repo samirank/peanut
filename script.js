1579a4eda864cd42ef907c9023df9b203f36b9f2
Samiran Kakoty <35275613+samirank@users.noreply.github.com>
2020-02-13 02:20:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Feb 12, 2020 00:00:00').getTime(),
+let countDown = new Date('Feb 15, 2020 19:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
